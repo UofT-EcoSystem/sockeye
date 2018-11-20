392e01ac02c611c757e515bdac2e32865c63ced4
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-20 16:33:53 -0500

    Changed from mxboard to tensorboard.

--- sockeye/callback.py
+++ sockeye/callback.py
@@ -64,22 +64,35 @@ class TrainingMonitor(object):
         self.start_tic = time.time()
         self.summary_writer = None
         if use_tensorboard:
-            import mxboard  # pylint: disable=import-error
+            # @ArmageddonKnight Changed from `mxboard` to `tensorboard`.
+            # import mxboard  # pylint: disable=import-error
+            try:
+                import tensorflow as tf
+            except ImportError:
+                logging.error("Please install tensorboard using `pip install tensorflow`.")
+
             log_dir = os.path.join(output_folder, C.TENSORBOARD_NAME)
             if os.path.exists(log_dir):
                 logger.info("Deleting existing tensorboard log dir %s", log_dir)
                 shutil.rmtree(log_dir)
             logger.info("Logging training events for Tensorboard at '%s'", log_dir)
-            self.summary_writer = mxboard.SummaryWriter(log_dir)
+            # self.summary_writer = mxboard.SummaryWriter(log_dir)
+            self.summary_writer = tf.summary.FileWriter(log_dir)
         self.cp_decoder = cp_decoder
         self.ctx = mp.get_context('spawn') # type: ignore
         self.num_concurrent_decodes = num_concurrent_decodes
         self.decoder_metric_queue = self.ctx.Queue()
         self.decoder_processes = []  # type: List[mp.Process]
         # TODO(fhieber): MXNet Speedometer uses root logger. How to fix this?
-        self.speedometer = mx.callback.Speedometer(batch_size=batch_size,
-                                                   frequent=C.MEASURE_SPEED_EVERY,
-                                                   auto_reset=False)
+        # @ArmageddonKnight Changed the callback subroutine from default `Speedometer` to `TensorboardSpeedometer`.
+        # `TensorboardSpeedometer` has the ability of logging Training Throughput, Memory Usage, and Evaluation Metrics.
+        # self.speedometer = mx.callback.Speedometer(batch_size=batch_size,
+        #                                            frequent=C.MEASURE_SPEED_EVERY,
+        #                                            auto_reset=False)
+        self.speedometer = mx.callback.TensorboardSpeedometer(summary_writer=self.summary_writer,
+                                                              batch_size=batch_size,
+                                                              frequent=C.MEASURE_SPEED_EVERY,
+                                                              auto_reset=False)
         utils.check_condition(optimized_metric in C.METRICS, "Unsupported metric: %s" % optimized_metric)
         if optimized_metric == C.BLEU:
             utils.check_condition(self.cp_decoder is not None, "%s requires CheckpointDecoder" % C.BLEU)
@@ -282,9 +295,14 @@ def write_tensorboard(summary_writer,
     :param metrics: Mapping of metric names to their values.
     :param checkpoint: Current checkpoint.
     """
-    import mxboard  # pylint: disable=import-error
+    # @ArmageddonKnight Changed from `mxboard` to `tensorboard`.
+    import tensorflow as tf # import mxboard  # pylint: disable=import-error
 
     for name, value in metrics.items():
-        summary_writer.add_scalar(tag=name, value=value,
-                                  global_step=checkpoint)
+
+        # summary_writer.add_scalar(tag=name, value=value,
+        #                           global_step=checkpoint)
+        self.summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag=name,
+                                                                           simple_value=value)]),
+                                        global_step=self.global_step)
 
