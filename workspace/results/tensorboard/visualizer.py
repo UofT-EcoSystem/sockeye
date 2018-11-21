3abff8105cbcc5caa1518573aa81d0c66c97acb1
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-21 01:53:02 -0500

    Minor fix in the order in which figures are plotted.

--- workspace/results/tensorboard/visualizer.py
+++ workspace/results/tensorboard/visualizer.py
@@ -72,9 +72,10 @@ if __name__ == "__main__":
     # setup the RC parameters
     plt_rc_setup()
 
-    plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='speed', metric_unit='Samples/s')
-    plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='speed', metric_unit='Samples/s')
     plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='perplexity')
     plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='perplexity')
     plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='memory_usage', metric_unit='GB')
     plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='memory_usage', metric_unit='GB')
+    plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='speed', metric_unit='Samples/s')
+    plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='speed', metric_unit='Samples/s')
+
