55888eace18e5d800b551ad09a94a1c37d03132d
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-20 19:33:53 -0500

    Reorganized files within the profile folder.

--- workspace/results/profile/memory/visualizer.py
+++ workspace/results/profile/memory/visualizer.py
@@ -158,10 +158,10 @@ if __name__ == "__main__":
     plt_memory_breakdown(sorted_stats_list=sorted_stats_list, 
                          expected_sum=4477.0 / 1024, 
                          xlabel="Layer Type",
-                         fig_name='sockeye-memory_profile-groundhog_iwslt15-layer.png')
+                         fig_name='sockeye-iwslt15-vi_en-groundhog-memory_profile-layer.png')
     sorted_stats_list = parse_memory_profile(memory_profile=args.memory_profile, 
                                              regex_dict=SOCKEYE_FUNCTION_REGEX_DICT)
     plt_memory_breakdown(sorted_stats_list=sorted_stats_list, 
                          expected_sum=4477.0 / 1024, 
                          xlabel="Data Structure",
-                         fig_name='sockeye-memory_profile-groundhog_iwslt15-function.png')
+                         fig_name='sockeye-iwslt15-vi_en-groundhog-memory_profile-function.png')
