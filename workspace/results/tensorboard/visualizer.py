c450cd6224b50b4f7f72a7b706a2cfc4ad4c945c
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-20 20:27:46 -0500

    Added the CSV files extracted from Tensorboard.

--- workspace/results/tensorboard/visualizer.py
+++ workspace/results/tensorboard/visualizer.py
@@ -10,8 +10,9 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--prefix', type=str, default=None)
-parser.add_argument('--metric', type=str, default=None)
+parser.add_argument('--csv-prefix', help='Prefix of CSV Files'
+                    type=str, default=None)
+parser.add_argument('--metric', help='Metric to be Plotted', type=str, default=None)
 
 
 def plt_rc_setup(dpi=400, fontsize=24):
@@ -28,13 +29,40 @@ def plt_rc_setup(dpi=400, fontsize=24):
     plt.rc('font', family='Times New Roman', size=fontsize)
 
 
-def plt_legacy_vs_partial_fw_prop(prefix, metric):
+def plt_legacy_vs_partial_fw_prop(csv_prefix, csv_suffix, metric,
+                                  xlabel='Global Step', 
+                                  ylabel=None, title=None):
     """
     Plot the comparison between legacy backpropagation and partial forward propagation.
 
     :param prefix: FileName Prefix
     :param metric: Metric recorded on Tensorboard
     """
+    if ylabel is None:
+        ylabel = metric.title().replace('_', ' ')
+    if title is None:
+        title ='%s-%s-legacy_vs_partial_fw_prop' % (csv_prefix, csv_suffix)
+
+    legacy  = np.genfromtxt(fname='%s-legacy-%s/csv/%s.csv' % (csv_prefix, csv_suffix, metric),
+                                    delimiter=',').astype(np.float64)[1:,:]
+    partial = np.genfromtxt(fname='%s-partial_fw_prop-%s/csv/%s.csv' % \
+                                          (csv_prefix, csv_suffix, metric),
+                                    delimiter=',').astype(np.float64)[1:,:]
+    
+    plt.Figure()
+
+    ax = plt.axes()
+    ax.plot(legacy [0], legacy [1], linewidth=1, linestyle='--', 
+            color='black', label='Legacy')
+    ax.plot(partial[0], partial[1], linewidth=1, linestype='-',
+            color='black', label='Partial FW Prop')
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig(title + ".png")
 
 
 if __name__ == "__main__":
