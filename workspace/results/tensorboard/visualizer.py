e96cc97f0a406de663d006b0a08a50fc2403d4cb
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-23 17:17:16 -0500

    Added the comparison graphs for batch size of 256.

--- workspace/results/tensorboard/visualizer.py
+++ workspace/results/tensorboard/visualizer.py
@@ -29,14 +29,15 @@ def gen_from_txt(fname):
     return data
 
 
-def plt_default_vs_econmt(csv_prefix, metric, metric_unit=None, ymin=None, ymax=None,
-                          xlabel='Global Step', 
-                          xlabel_unit='Number of Training Batches'):
+def plt_default_vs_econmt_preliminary(csv_prefix, metric, metric_unit=None):
     """
-    Plot the comparison between legacy backpropagation and partial forward propagation.
+    Plot the comparison between legacy backpropagation and 
+    partial forward propagation (First 500 Updates, Preliminary Ver.).
     """
+    xlabel = 'Global Step (Number of Training Batches)'
+
     ylabel = metric.title().replace('_', ' ')
-    title ='%s-default_vs_econmt-%s' % (csv_prefix, metric)
+    title ='%s-default_vs_econmt-preliminary-%s' % (csv_prefix, metric)
 
     default = gen_from_txt(fname='%s-default/csv/%s.csv' % (csv_prefix, metric))
     econmt  = gen_from_txt(fname= '%s-econmt/csv/%s.csv' % (csv_prefix, metric))
@@ -45,6 +46,8 @@ def plt_default_vs_econmt(csv_prefix, metric, metric_unit=None, ymin=None, ymax=
         default[:,2] = default[:,2] / 1000
         econmt [:,2] = econmt [:,2] / 1000
     
+    # ==============================================================================================
+
     plt.figure()
 
     plt.plot(default[:,1], default[:,2], linewidth=2, linestyle='--', 
@@ -52,7 +55,7 @@ def plt_default_vs_econmt(csv_prefix, metric, metric_unit=None, ymin=None, ymax=
     plt.plot(econmt [:,1], econmt [:,2], linewidth=2, linestyle='-',
              color='black', label='EcoNMT')
 
-    plt.xlabel("%s (%s)" % (xlabel, xlabel_unit))
+    plt.xlabel(xlabel)
     plt.ylabel("%s (%s)" % (ylabel, metric_unit) if metric_unit is not None else ylabel)
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
@@ -82,6 +85,8 @@ def plt_throughput_vs_batch_size():
         sockeye_memory_usage.append(gen_from_txt("iwslt15-vi_en-tbd-500-default-B_%d/csv/memory_usage.csv" % batch_size)[-1, 2])
 
     sockeye_memory_usage = [memory_usage / 1000 for memory_usage in sockeye_memory_usage]
+
+    # ==============================================================================================
 
     plt.figure()
 
@@ -119,6 +124,8 @@ def plt_throughput_vs_batch_size():
 
     axes.grid(linestyle='-.', linewidth=1)
 
+    # ==============================================================================================
+
     axes = axes.twinx()
 
     memory_usage_plot = axes.plot(B, sockeye_memory_usage, linewidth=2, linestyle='--',
@@ -134,16 +141,75 @@ def plt_throughput_vs_batch_size():
     plt.savefig("throughput_and_memory_usage_vs_batch_size-sockeye.png")
 
 
+def plt_default_vs_econmt_full_training(csv_prefix, xscale, metric, metric_unit=None):
+    """
+    Plot the comparison between legacy backpropagation and 
+    partial forward propagation (Full Training Ver.).
+    """
+    if xscale == 'N':
+        xlabel = 'Global Step (Number of Training Batches)'
+    elif xscale == 'T':
+        xlabel = r'Time ($\text{ms}$)'
+    else:
+        assert False, "Invalid xlabel %s. It must be either \'N\' or \'T\'."
+    
+    ylabel = metric.title().replace('_', ' ')
+    title ='%s-default_vs_econmt-full_training-%s' % (csv_prefix, metric)
+
+    default_128_metric = gen_from_txt("%s-default-B_128/csv/%s.csv" % (csv_prefix, metric))
+    econmt_128_metric  = gen_from_txt( "%s-econmt-B_128/csv/%s.csv" % (csv_prefix, metric))
+    econmt_256_metric  = gen_from_txt( "%s-econmt-B_256/csv/%s.csv" % (csv_prefix, metric))
+
+    if metric == 'memory_usage' and metric_unit == 'GB':
+        default_128_metric[:,2] = default_128_metric[:,2] / 1000
+        econmt_128_metric [:,2] = econmt_128_metric [:,2] / 1000
+        econmt_256_metric [:,2] = econmt_256_metric [:,2] / 1000
+
+    # ==============================================================================================
+
+    plt.figure()
+
+    plt.plot(default_128_metric[:,1] if xscale == 'N' else default_128_metric[:,0], 
+             default_128_metric[:,2], linewidth=2, linestyle='--', 
+             color='black', label='Default')
+    plt.plot(econmt_128_metric [:,1] if xscale == 'N' else econmt_128_metric [:,0], 
+             econmt_128_metric [:,2], linewidth=2, linestyle='-',
+             color='black', label='EcoNMT')
+    plt.plot(econmt_256_metric [:,1] if xscale == 'N' else econmt_256_metric [:,0], 
+             econmt_256_metric [:,2], linewidth=2, linestyle='-',
+             color='green', label='EcoNMT')
+
+    plt.xlabel(r'Time (min)')
+    plt.ylabel("%s (%s)" % (ylabel, metric_unit) if metric_unit is not None else ylabel)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+
+    if metric == 'memory_usage':
+        plt.yticks(np.arange(0, 13, 4))
+    if metric == 'perplexity':
+        plt.yticks(np.arange(0, 1300, 400))
+
+    plt.legend(fontsize=20)
+    plt.grid(linestyle='-.', linewidth=1)
+
+    plt.tight_layout()
+    plt.savefig(title + ".png")
+
 
 if __name__ == "__main__":
     # setup the RC parameters
     plt_rc_setup()
 
-    # plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='perplexity')
-    # plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='perplexity')
-    # plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='memory_usage', metric_unit='GB')
-    # plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='memory_usage', metric_unit='GB')
-    # plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-groundhog-500', metric='throughput', metric_unit='Samples/s')
-    # plt_default_vs_econmt(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='throughput', metric_unit='Samples/s')
+    # plt_default_vs_econmt_preliminary(csv_prefix='iwslt15-vi_en-groundhog-500', metric='perplexity')
+    # plt_default_vs_econmt_preliminary(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='perplexity')
+    # plt_default_vs_econmt_preliminary(csv_prefix='iwslt15-vi_en-groundhog-500', metric='memory_usage', metric_unit='GB')
+    # plt_default_vs_econmt_preliminary(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='memory_usage', metric_unit='GB')
+    # plt_default_vs_econmt_preliminary(csv_prefix='iwslt15-vi_en-groundhog-500', metric='throughput', metric_unit='Samples/s')
+    # plt_default_vs_econmt_preliminary(csv_prefix='iwslt15-vi_en-tbd-500'      , metric='throughput', metric_unit='Samples/s')
 
-    plt_throughput_vs_batch_size()
+    # plt_throughput_vs_batch_size()
+
+    plt_default_vs_econmt_full_training(csv_prefix='iwslt15-vi_en-tbd-10k', xscale='T', metric='perplexity')
+    plt_default_vs_econmt_full_training(csv_prefix='iwslt15-vi_en-tbd-10k', xscale='T', metric='memory_usage')
+    plt_default_vs_econmt_full_training(csv_prefix='iwslt15-vi_en-tbd-10k', xscale='T', metric='throughput')
+    plt_default_vs_econmt_full_training(csv_prefix='iwslt15-vi_en-tbd-10k', xscale='T', metric='validation_bleu')
