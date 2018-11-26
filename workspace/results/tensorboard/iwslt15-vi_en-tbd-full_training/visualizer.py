f15565210ba4255c53e870a7c11f094b55865c31
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-26 02:41:06 -0500

    Updated the graphs for the validation BLEU score.

--- workspace/results/tensorboard/iwslt15-vi_en-tbd-full_training/visualizer.py
+++ workspace/results/tensorboard/iwslt15-vi_en-tbd-full_training/visualizer.py
@@ -16,40 +16,35 @@ plt_rc_setup()
 # ==================================================================================================
 
 
-def plt_default_vs_econmt_full_training_validation_bleu(xscale):
+def plt_default_vs_econmt_full_training_validation_bleu(xscale, par_rev):
 
     metric, metric_unit = 'validation_bleu', None
 
     if xscale != 'N' and xscale != 'T':
         assert False, "Invalid xlabel %s. It must be either \'N\' or \'T\'."
     
-    ylabel = metric.title().replace('_', ' ')
-    title ='default_vs_econmt-%s-%s' % (xscale, metric)
+    title ='default_vs_econmt%s-%s-%s' % ('-par_rev' if par_rev else '', xscale, metric)
 
-    default_128_metric = gen_from_txt("default-B_128/csv/%s.csv" % metric, metric, metric_unit)
-    # econmt_128_metric  = gen_from_txt( "econmt-B_128/csv/%s.csv" % metric, metric, metric_unit)
-    econmt_256_metric  = gen_from_txt( "econmt-B_256/csv/%s.csv" % metric, metric, metric_unit)
-
-    default_128_par_rev_metric = gen_from_txt("default-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit)
-    econmt_128_par_rev_metric  = gen_from_txt( "econmt-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit)
-    econmt_256_par_rev_metric  = gen_from_txt( "econmt-B_256-par_rev/csv/%s.csv" % metric, metric, metric_unit)
+    default_128_metric = gen_from_txt("default-B_128%s/csv/%s.csv" % ('-par_rev' if par_rev else '', metric), metric, metric_unit)
+    econmt_128_metric  = gen_from_txt( "econmt-B_128%s/csv/%s.csv" % ('-par_rev' if par_rev else '', metric), metric, metric_unit)
+    econmt_256_metric  = gen_from_txt( "econmt-B_256%s/csv/%s.csv" % ('-par_rev' if par_rev else '', metric), metric, metric_unit)
 
     # ==============================================================================================
 
     plt.figure()
 
-    plt.plot(default_128_par_rev_metric[:9,1] if xscale == 'N' else default_128_par_rev_metric[:9,0], 
-             default_128_par_rev_metric[:9,2], linewidth=2, linestyle='-', 
+    plt.plot(default_128_metric[:9,1] if xscale == 'N' else default_128_metric[:9,0], 
+             default_128_metric[:9,2], linewidth=2, linestyle='-', 
              marker='x', markersize=10,
-             color='black', label=r'Default$_{B=128}^{\mathrm{par\_rev}}$')
-    plt.plot(econmt_128_par_rev_metric [:7,1] if xscale == 'N' else econmt_128_par_rev_metric [:7,0], 
-             econmt_128_par_rev_metric [:7,2], linewidth=2, linestyle='-', 
+             color='black', label=r'Default$_{B=128}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
+    plt.plot(econmt_128_metric [:7,1] if xscale == 'N' else econmt_128_metric [:7,0], 
+             econmt_128_metric [:7,2], linewidth=2, linestyle='-', 
              marker='.', markersize=10,
-             color='black', label= r'EcoNMT$_{B=128}^{\mathrm{par\_rev}}$')
-    plt.plot(econmt_256_par_rev_metric [:6,1] if xscale == 'N' else econmt_256_par_rev_metric [:6,0], 
-             econmt_256_par_rev_metric [:6,2], linewidth=2, linestyle='-', 
+             color='black', label= r'EcoNMT$_{B=128}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
+    plt.plot(econmt_256_metric [:6,1] if xscale == 'N' else econmt_256_metric [:6,0], 
+             econmt_256_metric [:6,2], linewidth=2, linestyle='-', 
              marker='+', markersize=10,
-             color='black', label= r'EcoNMT$_{B=256}^{\mathrm{par\_rev}}$')
+             color='black', label= r'EcoNMT$_{B=256}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
 
     plt.axhline(y=22.6, color='r', linewidth=2, linestyle='-.')
 
@@ -67,5 +62,8 @@ def plt_default_vs_econmt_full_training_validation_bleu(xscale):
     plt.tight_layout()
     plt.savefig(title + ".png")
 
-plt_default_vs_econmt_full_training_validation_bleu('N')
-plt_default_vs_econmt_full_training_validation_bleu('T')
+plt_default_vs_econmt_full_training_validation_bleu('N', False)
+plt_default_vs_econmt_full_training_validation_bleu('T', False)
+plt_default_vs_econmt_full_training_validation_bleu('N', True)
+plt_default_vs_econmt_full_training_validation_bleu('T', True)
+
