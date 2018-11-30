f6c9e002604142d8e39df8c7d72eef752b6308b2
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-11-30 14:58:43 -0500

    Redrew the full training graphs.

--- workspace/results/tensorboard/iwslt15-vi_en-tbd-full_training/visualizer.py
+++ workspace/results/tensorboard/iwslt15-vi_en-tbd-full_training/visualizer.py
@@ -35,12 +35,19 @@ def plt_default_vs_econmt_full_training_validation_bleu(xscale, par_rev):
 
     first_k_ckpts = 7 if par_rev else 8
 
+    if par_rev:
+        default_128_raw_metric = gen_from_txt("default-B_128/csv/%s.csv" % metric, 
+                                              metric, metric_unit)
+        plt.plot(default_128_raw_metric[:first_k_ckpts,1] if xscale == 'N' else default_128_raw_metric[:first_k_ckpts,0], 
+                 default_128_raw_metric[:first_k_ckpts,2], linewidth=2, linestyle='-', 
+                 marker='v', markersize=10,
+                 color='black', label=r'Default$_{B=128}$')
     plt.plot(default_128_metric[:first_k_ckpts,1] if xscale == 'N' else default_128_metric[:first_k_ckpts,0], 
              default_128_metric[:first_k_ckpts,2], linewidth=2, linestyle='-', 
              marker='X', markersize=10,
              color='black', label=r'Default$_{B=128}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
     plt.plot(econmt_128_metric [:first_k_ckpts,1] if xscale == 'N' else econmt_128_metric [:first_k_ckpts,0], 
-             econmt_128_metric [:first_k_ckpts,2], linewidth=2, linestyle='--', 
+             econmt_128_metric [:first_k_ckpts,2], linewidth=2, linestyle='-', 
              marker='.', markersize=10,
              color='black', label= r'EcoNMT$_{B=128}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
     plt.plot(econmt_256_metric [:6,1] if xscale == 'N' else econmt_256_metric [:6,0], 
@@ -86,19 +93,24 @@ def plt_default_vs_econmt_full_training_perplexity(xscale, par_rev):
 
     first_k_ckpts = 100
 
-    if par_rev
-
+    if par_rev:
+        default_128_raw_metric = gen_from_txt("default-B_128/csv/%s.csv" % metric, 
+                                              metric, metric_unit)
+        plt.plot(default_128_raw_metric[:first_k_ckpts,1] if xscale == 'N' else default_128_raw_metric[:first_k_ckpts,0], 
+                 default_128_raw_metric[:first_k_ckpts,2], linewidth=2, linestyle='-', 
+                 marker='v', markersize=10, markevery=(1, 10),
+                 color='black', label=r'Default$_{B=128}$')
     plt.plot(default_128_metric[:first_k_ckpts,1] if xscale == 'N' else default_128_metric[:first_k_ckpts,0], 
              default_128_metric[:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='X', markersize=10, markevery=10,
+             marker='X', markersize=10, markevery=(3, 10),
              color='black', label=r'Default$_{B=128}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
     plt.plot(econmt_128_metric [:first_k_ckpts,1] if xscale == 'N' else econmt_128_metric [:first_k_ckpts,0], 
-             econmt_128_metric [:first_k_ckpts,2], linewidth=2, linestyle='--', 
-             marker='.', markersize=10, markevery=10,
+             econmt_128_metric [:first_k_ckpts,2], linewidth=2, linestyle='-', 
+             marker='.', markersize=10, markevery=(5, 10),
              color='black', label= r'EcoNMT$_{B=128}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
     plt.plot(econmt_256_metric [:first_k_ckpts,1] if xscale == 'N' else econmt_256_metric [:first_k_ckpts,0], 
              econmt_256_metric [:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='^', markersize=10, markevery=10,
+             marker='^', markersize=10, markevery=(7, 10),
              color='black', label= r'EcoNMT$_{B=256}%s$' % ('^{\mathrm{par\_rev}}' if par_rev else ''))
 
     plt.xlabel('Global Step' if xscale == 'N' else 'Time (min)')
@@ -151,17 +163,17 @@ def plt_default_vs_econmt_full_training_end2end():
     plt.savefig(title + ".png")
 
 
-def plt_default_vs_econmt_full_training_throughput(metric, metric_unit, measurer, ylabel,
-                                                   bar_width=0.3):
+def plt_default_vs_econmt_full_training_metrics(metric, metric_unit, measurer, ylabel,
+                                                bar_width=0.3):
 
     title ='default_vs_econmt-%s' % metric
 
     default_128_metric = gen_from_txt("default-B_128/csv/%s.csv" % metric,
                                       metric=metric, metric_unit=metric_unit, skip=40)
-    econmt_128_metric  = gen_from_txt( "econmt-B_128/csv/%s.csv" % metric, 
-                                      metric=metric, metric_unit=metric_unit, skip=40)
-    econmt_256_metric  = gen_from_txt( "econmt-B_256/csv/%s.csv" % metric, 
-                                      metric=metric, metric_unit=metric_unit, skip=20)
+    # econmt_128_metric  = gen_from_txt( "econmt-B_128/csv/%s.csv" % metric, 
+    #                                   metric=metric, metric_unit=metric_unit, skip=40)
+    # econmt_256_metric  = gen_from_txt( "econmt-B_256/csv/%s.csv" % metric, 
+    #                                   metric=metric, metric_unit=metric_unit, skip=20)
     default_128_par_rev_metric = gen_from_txt("default-B_128-par_rev/csv/%s.csv" % metric,
                                               metric=metric, metric_unit=metric_unit, skip=40)
     econmt_128_par_rev_metric  = gen_from_txt( "econmt-B_128-par_rev/csv/%s.csv" % metric,
@@ -172,61 +184,61 @@ def plt_default_vs_econmt_full_training_throughput(metric, metric_unit, measurer
     # ==============================================================================================
 
     default_128_metric = measurer(default_128_metric[:,2])
-    econmt_128_metric  = measurer( econmt_128_metric[:,2])
-    econmt_256_metric  = measurer( econmt_256_metric[:,2])
+    # econmt_128_metric  = measurer( econmt_128_metric[:,2])
+    # econmt_256_metric  = measurer( econmt_256_metric[:,2])
     default_128_par_rev_metric = measurer(default_128_par_rev_metric[:,2])
     econmt_128_par_rev_metric  = measurer( econmt_128_par_rev_metric[:,2])
     econmt_256_par_rev_metric  = measurer( econmt_256_par_rev_metric[:,2])
 
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(6, 8))
     # plt.figure()
 
     def _annotate(x, metric):
         plt.annotate((r'$%.1f\times$') % (metric / default_128_par_rev_metric),
                  xy    =(x, metric + 0.04*plt.ylim()[1]), 
                  xytext=(x, metric + 0.04*plt.ylim()[1]), 
-                 fontsize=17, ha='center', va='center', 
+                 fontsize=20, ha='center', va='center', 
                  bbox=dict(boxstyle='square', facecolor='white', linewidth=3))
 
     handles = []
 
-    handles.append(plt.bar(x=-2.5*bar_width, height=default_128_metric,
-            width=bar_width, edgecolor='black', linewidth=3,
-            color= 'grey',
-            label=r"Default$_{B=128}$"))
-    handles.append(plt.bar(x=-1.5*bar_width, height= econmt_128_metric,
-            width=bar_width, edgecolor='black', linewidth=3,
-            color='white',
-            label= r"EcoRNN$_{B=128}$"))
-    handles.append(plt.bar(x=-0.5*bar_width, height= econmt_256_metric,
-            width=bar_width, edgecolor='black', linewidth=3,
-            color='green',
-            label= r"EcoRNN$_{B=128}$"))
-    handles.append(plt.bar(x= 0.5*bar_width, height=default_128_par_rev_metric,
+    handles.append(plt.bar(x=-1.5*bar_width, height=default_128_metric,
             width=bar_width, edgecolor='black', linewidth=3,
             color= 'grey', hatch='/',
+            label=r"Default$_{B=128}$"))
+    # handles.append(plt.bar(x=-1.5*bar_width, height= econmt_128_metric,
+    #         width=bar_width, edgecolor='black', linewidth=3,
+    #         color='white',
+    #         label= r"EcoRNN$_{B=128}$"))
+    # handles.append(plt.bar(x=-0.5*bar_width, height= econmt_256_metric,
+    #         width=bar_width, edgecolor='black', linewidth=3,
+    #         color='green',
+    #         label= r"EcoRNN$_{B=128}$"))
+    handles.append(plt.bar(x=-0.5*bar_width, height=default_128_par_rev_metric,
+            width=bar_width, edgecolor='black', linewidth=3,
+            color= 'grey', 
             label=r"Default$_{B=128}^\mathrm{par\_rev}$"))
-    handles.append(plt.bar(x= 1.5*bar_width, height= econmt_128_par_rev_metric,
+    handles.append(plt.bar(x= 0.5*bar_width, height= econmt_128_par_rev_metric,
             width=bar_width, edgecolor='black', linewidth=3,
-            color='white', hatch='/',
+            color='white', 
             label= r"EcoRNN$_{B=128}^\mathrm{par\_rev}$"))
-    handles.append(plt.bar(x= 2.5*bar_width, height= econmt_256_par_rev_metric,
+    handles.append(plt.bar(x= 1.5*bar_width, height= econmt_256_par_rev_metric,
             width=bar_width, edgecolor='black', linewidth=3,
-            color='green', hatch='/',
+            color='green', 
             label= r"EcoRNN$_{B=256}^\mathrm{par\_rev}$"))
 
-    _annotate(x=-2.5*bar_width, metric=default_128_metric)
-    _annotate(x=-1.5*bar_width, metric= econmt_128_metric)
-    _annotate(x=-0.5*bar_width, metric= econmt_256_metric)
-    _annotate(x= 0.5*bar_width, metric=default_128_par_rev_metric)
-    _annotate(x= 1.5*bar_width, metric= econmt_128_par_rev_metric)
-    _annotate(x= 2.5*bar_width, metric= econmt_256_par_rev_metric)
+    _annotate(x=-1.5*bar_width, metric=default_128_metric)
+    # _annotate(x=-1.5*bar_width, metric= econmt_128_metric)
+    # _annotate(x=-0.5*bar_width, metric= econmt_256_metric)
+    _annotate(x=-0.5*bar_width, metric=default_128_par_rev_metric)
+    _annotate(x= 0.5*bar_width, metric= econmt_128_par_rev_metric)
+    _annotate(x= 1.5*bar_width, metric= econmt_256_par_rev_metric)
 
     plt.xticks([])
     plt.yticks(fontsize=20)
     plt.ylabel(ylabel, fontsize=32)
 
-    # plt.legend(fontsize=20, ncol=1, loc=(1, 0.0))
+    # plt.legend(fontsize=20, ncol=1)
     plt.grid(linestyle='-.', linewidth=1)
 
     plt.tight_layout()
@@ -245,11 +257,11 @@ plt_default_vs_econmt_full_training_validation_bleu('T', False)
 plt_default_vs_econmt_full_training_validation_bleu('N', True)
 plt_default_vs_econmt_full_training_validation_bleu('T', True)
 
-plt_default_vs_econmt_full_training_end2end()
+# plt_default_vs_econmt_full_training_end2end()
 
-plt_default_vs_econmt_full_training_throughput(metric='throughput', metric_unit='samples/s', 
-                                               measurer=np.average,
-                                               ylabel='Avg Throughput (samples/s)')
-plt_default_vs_econmt_full_training_throughput(metric='memory_usage', metric_unit='GB', 
-                                               measurer=np.max, 
-                                               ylabel='Max Memory Consumption (GB)',)
+plt_default_vs_econmt_full_training_metrics(metric='throughput', metric_unit='samples/s', 
+                                            measurer=np.average,
+                                            ylabel='Avg Throughput (samples/s)')
+plt_default_vs_econmt_full_training_metrics(metric='memory_usage', metric_unit='GB', 
+                                             measurer=np.max, 
+                                            ylabel='Max Memory Consumption (GB)',)
