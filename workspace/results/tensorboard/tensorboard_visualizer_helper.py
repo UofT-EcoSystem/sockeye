22ced369a71f7eb74b1cd57c8b2f6c23e8c94100
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-12-03 13:49:29 -0500

    Updated the graphs.

--- workspace/results/tensorboard/tensorboard_visualizer_helper.py
+++ workspace/results/tensorboard/tensorboard_visualizer_helper.py
@@ -83,12 +83,11 @@ def plt_default_vs_econmt_full_training_validation_bleu(xscale, first_k_ckpts,
     if xscale != 'N' and xscale != 'T':
         assert False, "Invalid xlabel %s. It must be either \'N\' or \'T\'."
     
-    title ='%sdefault_vs_econmt-par_rev-%s-%s' % (prefix, xscale, metric)
+    title ='%sdefault_vs_econmt%s-par_rev-%s-%s' % (prefix, suffix, xscale, metric)
 
     default_128_raw_metric = gen_from_txt("default-B_128/csv/%s.csv" % metric, metric, metric_unit)
     default_128_metric = gen_from_txt("default-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit, 
                                       discard[0] if discard is not None else None)
-    cudnn_128_metric   = gen_from_txt(  "cudnn-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit)
     econmt_128_metric  = gen_from_txt( "econmt-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit,
                                       discard[0] if discard is not None else None)
     econmt_256_metric  = gen_from_txt( "econmt-B_256-par_rev/csv/%s.csv" % metric, metric, metric_unit,
@@ -103,46 +102,42 @@ def plt_default_vs_econmt_full_training_validation_bleu(xscale, first_k_ckpts,
     handles.append(plt.plot(default_128_raw_metric[:first_k_ckpts[0],1] if xscale == 'N' \
                                 else default_128_raw_metric[:first_k_ckpts[0],0], 
                             default_128_raw_metric[:first_k_ckpts[0],2], linewidth=2, linestyle='-', 
-                            marker='v', markersize=5,
+                            marker='v', markersize=10,
                             color='black', label=r'Default$_{B=128}$')[0])
     handles.append(plt.plot(default_128_metric[:first_k_ckpts[1],1] if xscale == 'N' \
                                 else default_128_metric[:first_k_ckpts[1],0], 
                             default_128_metric[:first_k_ckpts[1],2], linewidth=2, linestyle='-', 
-                            marker='X', markersize=5,
+                            marker='X', markersize=10,
                             color='black', label=r'Default$_{B=128}^{\mathrm{par\_rev}}$')[0])
-    handles.append(plt.plot(cudnn_128_metric  [:first_k_ckpts[2],1] if xscale == 'N' \
-                                else   cudnn_128_metric[:first_k_ckpts[2],0], 
-                            cudnn_128_metric  [:first_k_ckpts[2],2], linewidth=2, linestyle='-', 
-                            marker='s', markersize=5,
-                            color='black', label=  r'CuDNN$_{B=128}^{\mathrm{par\_rev}}$')[0])
-    handles.append(plt.plot(econmt_128_metric [:first_k_ckpts[3],1] if xscale == 'N' \
-                                else econmt_128_metric [:first_k_ckpts[3],0], 
-                            econmt_128_metric [:first_k_ckpts[3],2], linewidth=2, linestyle='-', 
-                            marker='o', markersize=5,
+    handles.append(plt.plot(econmt_128_metric [:first_k_ckpts[2],1] if xscale == 'N' \
+                                else econmt_128_metric [:first_k_ckpts[2],0], 
+                            econmt_128_metric [:first_k_ckpts[2],2], linewidth=2, linestyle='-', 
+                            marker='.', markersize=10,
                             color='black', label= r'EcoRNN$_{B=128}^{\mathrm{par\_rev}}$')[0])
-    handles.append(plt.plot(econmt_256_metric [:first_k_ckpts[4],1] if xscale == 'N' \
-                                else econmt_256_metric[:first_k_ckpts[4],0], 
-                            econmt_256_metric [:first_k_ckpts[4],2], linewidth=2, linestyle='-', 
-                            marker='^', markersize=5, 
+    handles.append(plt.plot(econmt_256_metric [:first_k_ckpts[3],1] if xscale == 'N' \
+                                else econmt_256_metric[:first_k_ckpts[3],0], 
+                            econmt_256_metric [:first_k_ckpts[3],2], linewidth=2, linestyle='-', 
+                            marker='^', markersize=10, 
                             color='black', label= r'EcoRNN$_{B=256}^{\mathrm{par\_rev}}$')[0])
 
     def _annotate(x, y):
         plt.annotate(r"$%.2f\times$" % (x / default_128_metric[first_k_ckpts[1]-2,0]), 
                     xy    =(0, y),
                     xytext=(x, y),
-                    fontsize=24,
+                    fontsize=20,
                     va="center", ha="left",
                     bbox=dict(boxstyle="square", fc="white", ec='blue', linewidth=3),
                     arrowprops=dict(arrowstyle="<|-|>", color='blue', linewidth=3))
     
     if xscale == 'T':
-        _annotate(default_128_raw_metric[first_k_ckpts[0]-2,0], 5*bar/6)
-        _annotate(default_128_metric[first_k_ckpts[1]-2,0], 4*bar/6)
-        _annotate(  cudnn_128_metric[first_k_ckpts[2]-2,0], 3*bar/6)
-        _annotate( econmt_128_metric[first_k_ckpts[3]-2,0], 2*bar/6)
-        _annotate( econmt_256_metric[first_k_ckpts[4]-2,0], 1*bar/6)
+        _annotate(default_128_raw_metric[first_k_ckpts[0]-2,0], 4*bar/5)
+        _annotate(default_128_metric[first_k_ckpts[1]-2,0], 3*bar/5)
+        _annotate( econmt_128_metric[first_k_ckpts[3]-2,0], 2*bar/5)
+        _annotate( econmt_256_metric[first_k_ckpts[4]-2,0], 1*bar/5)
 
     if bar is not None:
+        plt.text(2, bar + 2, 'Target BLEU Score %.1f' % bar, fontsize=18,
+                 bbox=dict(boxstyle="square", fc="white", ec='red', linewidth=3))
         plt.axhline(y=bar, color='r', linewidth=2, linestyle='-.')
 
     plt.xlabel('Training Checkpoint Number' if xscale == 'N' else 'Time (min)')
@@ -174,7 +169,6 @@ def plt_default_vs_econmt_full_training_perplexity(xscale, prefix='', suffix='')
 
     default_128_raw_metric = gen_from_txt("default-B_128/csv/%s.csv" % metric, metric, metric_unit)
     default_128_metric = gen_from_txt("default-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit)
-    cudnn_128_metric   = gen_from_txt(  "cudnn-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit)
     econmt_128_metric  = gen_from_txt( "econmt-B_128-par_rev/csv/%s.csv" % metric, metric, metric_unit)
     econmt_256_metric  = gen_from_txt( "econmt-B_256-par_rev/csv/%s.csv" % metric, metric, metric_unit)
 
@@ -183,27 +177,22 @@ def plt_default_vs_econmt_full_training_perplexity(xscale, prefix='', suffix='')
     plt.figure()
 
     first_k_ckpts = 100
-
         
     plt.plot(default_128_raw_metric[:first_k_ckpts,1] if xscale == 'N' else default_128_raw_metric[:first_k_ckpts,0], 
              default_128_raw_metric[:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='v', markersize=10, markevery=(0, 20),
+             marker='v', markersize=10, markevery=(1, 10),
              color='black', label=r'Default$_{B=128}$')
     plt.plot(default_128_metric[:first_k_ckpts,1] if xscale == 'N' else default_128_metric[:first_k_ckpts,0], 
              default_128_metric[:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='X', markersize=10, markevery=(4, 20),
+             marker='X', markersize=10, markevery=(3, 10),
              color='black', label=r'Default$_{B=128}^{\mathrm{par\_rev}}$')
-    plt.plot(cudnn_128_metric  [:first_k_ckpts,1] if xscale == 'N' else cudnn_128_metric  [:first_k_ckpts,0], 
-             cudnn_128_metric  [:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='s', markersize=10, markevery=(8, 20),
-             color='black', label=  r'CuDNN$_{B=128}^{\mathrm{par\_rev}}$')
     plt.plot(econmt_128_metric [:first_k_ckpts,1] if xscale == 'N' else econmt_128_metric [:first_k_ckpts,0], 
              econmt_128_metric [:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='o', markersize=10, markevery=(12, 20),
+             marker='.', markersize=10, markevery=(5, 10),
              color='black', label= r'EcoRNN$_{B=128}^{\mathrm{par\_rev}}$')
     plt.plot(econmt_256_metric [:first_k_ckpts,1] if xscale == 'N' else econmt_256_metric [:first_k_ckpts,0], 
              econmt_256_metric [:first_k_ckpts,2], linewidth=2, linestyle='-', 
-             marker='^', markersize=10, markevery=(16, 20),
+             marker='^', markersize=10, markevery=(7, 10),
              color='black', label= r'EcoRNN$_{B=256}^{\mathrm{par\_rev}}$')
 
     plt.xlabel('Global Step' if xscale == 'N' else 'Time (min)')
@@ -229,8 +218,6 @@ def plt_default_vs_econmt_full_training_metrics(metric, metric_unit, measurer, y
                                       metric=metric, metric_unit=metric_unit, skip=40)
     default_128_par_rev_metric = gen_from_txt("default-B_128-par_rev/csv/%s.csv" % metric,
                                               metric=metric, metric_unit=metric_unit, skip=40)
-    cudnn_128_par_rev_metric   = gen_from_txt("cudnn-B_128-par_rev/csv/%s.csv"   % metric,
-                                              metric=metric, metric_unit=metric_unit)
     econmt_128_par_rev_metric  = gen_from_txt("econmt-B_128-par_rev/csv/%s.csv"  % metric,
                                               metric=metric, metric_unit=metric_unit, skip=40)
     econmt_256_par_rev_metric  = gen_from_txt("econmt-B_256-par_rev/csv/%s.csv"  % metric,
@@ -240,7 +227,6 @@ def plt_default_vs_econmt_full_training_metrics(metric, metric_unit, measurer, y
 
     default_128_metric = measurer(default_128_metric[:,2])
     default_128_par_rev_metric = measurer(default_128_par_rev_metric[:,2])
-    cudnn_128_par_rev_metric   = measurer(cudnn_128_par_rev_metric  [:,2])
     econmt_128_par_rev_metric  = measurer(econmt_128_par_rev_metric [:,2])
     econmt_256_par_rev_metric  = measurer(econmt_256_par_rev_metric [:,2])
 
@@ -264,26 +250,21 @@ def plt_default_vs_econmt_full_training_metrics(metric, metric_unit, measurer, y
             width=bar_width, edgecolor='black', linewidth=3,
             color='white', 
             label=r"Default$_{B=128}^\mathrm{par\_rev}$"))
-    handles.append(plt.bar(x=0, height=cudnn_128_par_rev_metric,
-            width=bar_width, edgecolor='black', linewidth=3,
-            color='white', 
-            label=  r"CuDNN$_{B=128}^\mathrm{par\_rev}$"))
-    handles.append(plt.bar(x= 1*bar_width, height= econmt_128_par_rev_metric,
+    handles.append(plt.bar(x= 0*bar_width, height= econmt_128_par_rev_metric,
             width=bar_width, edgecolor='black', linewidth=3,
             color=np.array([0, 0.2, 0]),
             label= r"EcoRNN$_{B=128}^\mathrm{par\_rev}$"))
-    handles.append(plt.bar(x= 2*bar_width, height= econmt_256_par_rev_metric,
+    handles.append(plt.bar(x= 1*bar_width, height= econmt_256_par_rev_metric,
             width=bar_width, edgecolor='black', linewidth=3,
             color=np.array([0, 0.8, 0]),
             label= r"EcoRNN$_{B=256}^\mathrm{par\_rev}$"))
 
     _annotate(x=-2*bar_width, metric=default_128_metric)
     _annotate(x=-1*bar_width, metric=default_128_par_rev_metric)
-    _annotate(x= 0          , metric=  cudnn_128_par_rev_metric)
-    _annotate(x= 1*bar_width, metric= econmt_128_par_rev_metric)
-    _annotate(x= 2*bar_width, metric= econmt_256_par_rev_metric)
+    _annotate(x= 0*bar_width, metric= econmt_128_par_rev_metric)
+    _annotate(x= 1*bar_width, metric= econmt_256_par_rev_metric)
 
-    # plt.xlim([-3*bar_width, 2*bar_width])
+    plt.xlim([-3*bar_width, 2*bar_width])
     plt.xticks([])
     plt.yticks(fontsize=20)
     plt.ylabel(ylabel)
