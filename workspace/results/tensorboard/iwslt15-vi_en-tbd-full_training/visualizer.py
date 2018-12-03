a72974474266aedb9dc7c99543492a5affaff581
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2018-12-03 14:04:52 -0500

    Removed the time expansion of perplexity.

--- workspace/results/tensorboard/iwslt15-vi_en-tbd-full_training/visualizer.py
+++ workspace/results/tensorboard/iwslt15-vi_en-tbd-full_training/visualizer.py
@@ -21,7 +21,6 @@ from tensorboard_visualizer_helper import gen_from_txt, plt_default_vs_econmt_fu
 plt_rc_setup()
 
 plt_default_vs_econmt_full_training_perplexity('N', prefix='iwslt15-vi_en-tbd-')
-plt_default_vs_econmt_full_training_perplexity('T', prefix='iwslt15-vi_en-tbd-')
 plt_default_vs_econmt_full_training_validation_bleu('T', first_k_ckpts=(7, 7, 7, 7, 6),
                                                     bar=22.6, prefix='iwslt15-vi_en-tbd-')
 
