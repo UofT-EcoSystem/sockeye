fbf149cb8a3d6a4753da3a9c64b3034ccc6ebc22
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-02-15 17:19:31 -0500

    Moved workspace contents to the parent directory.

--- workspace/data/wmt16-de_en/download.sh
+++
@@ -1,7 +0,0 @@
-# This file downloads the IWSLT15 English-Vietnamese dataset. 
-
-DATASET=wmt16-de_en
-DATASET_ROOT=$(cd $(dirname $0) && pwd)
-
-cd ${DATASET_ROOT}; wget http://www.cs.toronto.edu/~bojian/Downloads/NMT/${DATASET}.tar.gz; \
-	tar xvzf ${DATASET}.tar.gz; rm -f ${DATASET}.tar.gz
